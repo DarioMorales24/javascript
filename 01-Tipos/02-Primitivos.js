3e3821fe363b6ea4d1b911b2a61d6952025e87f9
DarioMorales24 <soulmates0127@gmail.com>
2025-01-28 22:37:18 -0300

    first commit

---
+++ 01-Tipos/02-Primitivos.js
@@ -0,0 +1,16 @@
+// dato Number
+let numero = 1;
+
+// dato String
+let texto = 'Hola Mundo';
+
+// dato Boolean
+let verdadero = true;
+let falso = false;
+
+// dato undefined
+let noDefinido;
+let undef = undefined;
+
+// dato null
+let nulo = null;
